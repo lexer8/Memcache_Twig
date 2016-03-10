18a4e7c5de9c93323a506a7e6b4cb884227fa7e4
Oleksandr Petrenko <Lexer8@mail.ru>
2016-03-10 03:19:31 +0200

    Revert "Install The Website"
    This reverts commit f1cf52e75c07610a4ab7c0dc998b29d597c12f0a.

--- configs/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Название веб-странички
-Config::set('site_name', 'Memcache + Twig');
-
-// Параметры подключения к базе данных
-Config::set('db.host', 'localhost');
-Config::set('db.user', 'root');
-Config::set('db.password', '');
-Config::set('db.name', 'books');
